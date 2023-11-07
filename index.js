2ed769a4cb653ad552b6735a35289640c6f0f254
Junaid Ali <gunaidali75@gmail.com>
2023-11-07 06:07:16 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// import inquirer from "inquirer";
+// let answers:{
+//     Sentance: string
+// } = await inquirer.prompt([
+//     {
+//         name: "Sentance",
+//         type: "input",
+//         message: "Enter you Sentance to Word Count: "
+//     }
+// ])
+// let Words = answers.Sentance.trim().split(" ")
+// console.log(`Your Sentance Word count ${Words.length}`)
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    {
+        name: "Sentance",
+        type: "input",
+        message: "Enter you Words for Count: "
+    }
+]);
+let Words = answers.Sentance.trim().split(" ");
+console.log(`Your Word count ${Words.length}`);
